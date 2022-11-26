1391ca2291fe92088d8b8cb6346273f7646720e6
jithendrabandlamuri <jithendrabandlamuri2103@gmail.com>
2022-11-26 20:17:10 +0530

    updated backened

---
+++ server_side/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const { FieldValue } = require('firebase-admin/firestore')
+const app = express()
+const port = 8383
+const { db } = require('./firebase.js')
+
+app.use(express.json())
+
+app.get('/friends', async (req, res) => {
+    const peopleRef = db.collection('people').doc('associates')
+    const doc = await peopleRef.get()
+    if (!doc.exists) {
+        return res.sendStatus(400)
+    }
+
+    res.status(200).send(doc.data())
+})
+
+app.post('/addfriend', async (req, res) => {
+    const { name, status } = req.body
+    const peopleRef = db.collection('people').doc('associates')
+    const res2 = await peopleRef.set({
+        [name]: status
+    }, { merge: true })
+    res.status(200).send(friends)
+})
+
+app.listen(port, () => console.log(`Server has started on port: ${port}`))
