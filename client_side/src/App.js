53be51c967e3ded18b9ba11e06e25b96ebc18f0c
Nishaambariya <70773490+Nishaambariya@users.noreply.github.com>
2022-11-27 17:02:36 +0530

    created navbar

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import NavbarComponent from "./components/NavbarComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <NavbarComponent/>
+      <Routes>
+        <Route path="/" element=<Home/> />
+        <Route path="/about" element =<About/> />
+        <Route exact path='/about' element={< About />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
