91d247008f06e0dda127a65139b0df2ce60ad1a2
jithendrabandlamuri <jithendrabandlamuri2103@gmail.com>
2022-11-28 16:02:59 +0530

    sellerform

---
+++ client_side/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SellProperty from './SellProperty'
+function Home() {
+  return (
+    <div>Home
+      <div><SellProperty></SellProperty></div>
+    </div>
+    
+  )
+}
+
+export default Home
