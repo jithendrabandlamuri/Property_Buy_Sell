b3d5c966ac253a64df30288f7f858a70b39a2d81
Nishaambariya <70773490+Nishaambariya@users.noreply.github.com>
2022-11-28 12:29:29 +0530

    dropdown changed

--- client_side/src/components/NavbarComponent.jsx
+++ client_side/src/components/NavbarComponent.jsx
@@ -1,35 +1,68 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import {Link} from 'react-router-dom';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import logo from "../download.jpeg"
 
 const NavbarComponent = () => {
-  const location = ["Andhra Pradesh","Arunachal Pradesh","  Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", "Haryana","Himachal Pradesh" , "Jammu and Kashmir", "Jharkhand", "Karnataka", "Kerala", "Madhya Pradesh","Maharashtra" ,"Manipur" , "Meghalaya", "Mizoram", "Nagaland", "Odisha (Orissa)","Punjab" ,"Rajasthan" , "Sikkim", "Tamil Nadu"];
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link className='nav-link' to="/">Home</Link>
-            <Link className='nav-link' to="/about">About</Link>
-            
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              {location.map(value=>(
-                  <NavDropdown.Item href="#action/3.2">
-                  {value}
-                </NavDropdown.Item>
-              ))}
-              
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
-}
 
-export default NavbarComponent
+    const location = [
+        "Andhra Pradesh",
+        "Arunachal Pradesh",
+        "  Assam",
+        "Bihar",
+        "Chhattisgarh",
+        "Goa",
+        "Gujarat",
+        "Haryana",
+        "Himachal Pradesh",
+        "Jammu and Kashmir",
+        "Jharkhand",
+        "Karnataka",
+        "Kerala",
+        "Madhya Pradesh",
+        "Maharashtra",
+        "Manipur",
+        "Meghalaya",
+        "Mizoram",
+        "Nagaland",
+        "Odisha (Orissa)",
+        "Punjab",
+        "Rajasthan",
+        "Sikkim",
+        "Tamil Nadu",
+    ];
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <img src={logo} height="42px" width="41px"></img>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link className="nav-link" to="/">
+                            Home
+                        </Link>
+                        <Link className="nav-link" to="/about">
+                            About
+                        </Link>
+
+                        <Form.Select>
+                        <option>Location</option>
+                          {
+                            location.map(data=>(
+                              <option value={data}>{data}</option>
+                            ))
+                          }
+                        </Form.Select>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavbarComponent;
