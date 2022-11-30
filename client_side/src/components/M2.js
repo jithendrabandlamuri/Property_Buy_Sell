0f2d3ba0758ff1a1ff2609b9d6c4091684058637
Sahana R <sahanarmurthy@gmail.com>
2022-11-30 19:37:30 +0530

    view specific properties based on user

--- client_side/src/components/M2.js
+++ client_side/src/components/M2.js
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore'
-import { auth, db } from '../firebase'
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { auth, db } from "../firebase";
 import { Row, Col, Container, Card, Button, Form } from "react-bootstrap";
 
-function M2({ location, budget, flatType, navLocation,propertyV }) {
-  const [userProperties, setUserProperties] = useState([])
-  const postCollectionRef = collection(db, "properties")
+function M2({ location, budget, flatType, navLocation, propertyV }) {
+  const [userProperties, setUserProperties] = useState([]);
+  const postCollectionRef = collection(db, "properties");
 
   useEffect(() => {
     const getProperties = async () => {
-      const data = await getDocs(postCollectionRef)
-      setUserProperties(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-    }
-    getProperties()
-  })
+      const data = await getDocs(postCollectionRef);
+      setUserProperties(
+        data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+    };
+    getProperties();
+  });
 
   const renderCard = (card, id) => {
     return (
-      <div>
+      <Col
+        className="d-flex"
+        style={{ marginTop: "10px", marginBottom: "10px" }}
+      >
         <Card style={{ width: "18rem" }} className="shadow-lg" key={id}>
           <Card.Img
             style={{ height: "300px" }}
@@ -25,52 +30,54 @@ function M2({ location, budget, flatType, navLocation,propertyV }) {
             src={card.image}
           />
           <Card.Body>
-            <Card.Title> {card.title}</Card.Title>
-            <p>
+            <Card.Title style={{ height: "35px", marginBottom: "0px" }}>
+              {card.title}
+            </Card.Title>
+            <p style={{ height: "30px" }}>
               <Card.Subtitle>{card.subtitle}</Card.Subtitle>
             </p>
             <Card.Text>
-              <b> {card.text}</b> <br />
-              <i> onwards </i>
-              <Button className="float-end" variant="primary">
+              <b style={{ height: "20px" }}> {card.text}</b> <br />
+              <i style={{ height: "10px" }}> onwards </i>
+              <Button
+                style={{ height: "45px", borderRadius: "0.5" }}
+                className="float-end"
+                variant="primary"
+              >
                 View Details
               </Button>
             </Card.Text>
           </Card.Body>
         </Card>
-      </div>
+      </Col>
     );
   };
 
   return (
-    <div>
-      <Container>
+    <Container>
+      <div className="mt-5" style={{ marginRight: "30px", marginLeft: "50px" }}>
         <Row className="justify-content-between">
-          <Col className="col-lg-4 d-flex justify-content-between align-items-stretch m-5 ">
-            {
-              userProperties.map((post) => {
-                if (post.propertyType === propertyV) {
-                  return (
-                    renderCard(post, post.id)
-                  )
-                }
-                else if (post.city === location && post.flatType === flatType && post.price === budget) {
-                  return (
-                    renderCard(post, post.id)
-                  )
-                }
-                else if (post.city === navLocation) {
-                  return (
-                    renderCard(post, post.id)
-                  )
-                }
-              })
-              // {setPropertyT("h")}
-            }
-          </Col>
+          {/* <Col className="col-lg-4 d-flex justify-content-between align-items-stretch m-5 "> */}
+          {
+            userProperties.map((post) => {
+              if (post.propertyType === propertyV) {
+                return renderCard(post, post.id);
+              } else if (
+                post.city === location &&
+                post.flatType === flatType &&
+                post.price === budget
+              ) {
+                return renderCard(post, post.id);
+              } else if (post.city === navLocation) {
+                return renderCard(post, post.id);
+              }
+            })
+            // {setPropertyT("h")}
+          }
+          {/* </Col> */}
         </Row>
-      </Container>
-    </div>
+      </div>
+    </Container>
   );
 }
 
--- client_side/src/firebase.js
+++ client_side/src/firebase.js
@@ -1,20 +1,20 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/storage';
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
 import "firebase/compat/auth";
-import "firebase/compat/firestore"
+import "firebase/compat/firestore";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCBN7nA448-dwOYbLqs9E-sLp0Y0LHNTrs",
-  authDomain: "mock-project-f70b1.firebaseapp.com",
-  projectId: "mock-project-f70b1",
-  storageBucket: "mock-project-f70b1.appspot.com",
-  messagingSenderId: "879827012697",
-  appId: "1:879827012697:web:2dcb70f99a33ec6164f548",
-  measurementId: "G-3N2G9Y41EM"
+  apiKey: "AIzaSyBp55M63LDOD4WUc5GD3phwQk8jj9NsfzY",
+  authDomain: "login-32fea.firebaseapp.com",
+  projectId: "login-32fea",
+  storageBucket: "login-32fea.appspot.com",
+  messagingSenderId: "846855944805",
+  appId: "1:846855944805:web:a82886cd4786d00451648c",
+  measurementId: "G-LTE818Q6N8",
 });
 
-const app = firebase
+const app = firebase;
 export const storageRef = firebase.storage().ref();
-export const db = firebase.firestore()
-export const auth = firebase.auth()
-export default app
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+export default app;
